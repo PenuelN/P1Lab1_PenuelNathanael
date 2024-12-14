6734c16388f1c0542ea19e0070bb1531af7b6472
PenuelN <penueln6127@student.faytechcc.edu>
2024-12-14 11:15:49 -0500

    Add files via upload

---
+++ P1Lab1_PenuelNathanael.py
@@ -0,0 +1,14 @@
+# Nathanael Penuel
+# 9/23/2024
+# P1Lab1 
+# Welcome Message
+
+
+
+
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110.")
